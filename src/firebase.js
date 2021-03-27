7c7f68c670ea490206637ba5a1a67ac7cb22dffa
Emaneliforp <kietchouam@gmail.com>
2021-03-27 16:00:54 -0700

    added pushScore() and getAllScore()

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,33 @@ const config = {
     databaseURL: process.env.REACT_APP_DATABASEURL
 };
 firebase.initializeApp(config);
-let firebaseApp = {DB: firebase.database()};
+
+let DB = firebase.database();
+
+function pushScore(scores){
+    DB.ref("allScore").once("value")
+        .catch(console.error)
+        .then(snapshot => {
+            let allScore = [];
+            if(snapshot.val()) allScore = snapshot.val();
+            allScore.push(scores);
+            DB.ref("allScore").set(allScore)
+                .catch(console.error)
+                .then(console.log("uploaded score!"))
+        })
+}
+
+function getAllScore(){
+    DB.ref("allScore").once("value")
+        .catch(console.error)
+        .then(snapshot => {
+            return snapshot.val();
+        })
+}
+
+let firebaseApp = {
+    pushScore: pushScore(scores),
+    getAllScore: getAllScore()
+};
 
 export default firebaseApp;
